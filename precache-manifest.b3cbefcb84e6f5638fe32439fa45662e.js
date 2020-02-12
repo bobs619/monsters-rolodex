0d02c4e9ea0bd645c6ce539fd9a0b47683e87707
Agonoy <BAgonoy@semprautilities.com>
2020-02-11 16:34:16 -0800

    Updates

---
+++ precache-manifest.b3cbefcb84e6f5638fe32439fa45662e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd39fe0934f6c4968e21741ad29eb470",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2d11c78a40dc8b3160a2",
+    "url": "/monsters-rolodex/static/css/main.87f9e982.chunk.css"
+  },
+  {
+    "revision": "01fa59518f48c0c9b0f6",
+    "url": "/monsters-rolodex/static/js/2.650d6c94.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.650d6c94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d11c78a40dc8b3160a2",
+    "url": "/monsters-rolodex/static/js/main.43e29c40.chunk.js"
+  },
+  {
+    "revision": "420e781ce2f2f4d61faa",
+    "url": "/monsters-rolodex/static/js/runtime-main.9cf5dfda.js"
+  }
+]);
